3464b5fd275a556f53cecae3667f8b106939e35b
linhay <158179948@qq.com>
2019-01-04 17:43:18 +0800

    update

---
+++ task.js
@@ -0,0 +1,29 @@
+const shell = require('shelljs')
+
+function updateHexo() {
+    shell.cd('/hexo/')
+    // shell.exec('git checkout .')
+    var stdout = shell.exec('git pull').stdout
+    if (stdout == 'Already up to date.\n') { return false }
+    return true
+}
+
+function updatePost() {
+    shell.cd('/hexo/source/')
+    // shell.exec('git checkout .')
+    var stdout = shell.exec('git pull').stdout
+    if (stdout == 'Already up to date.\n') { return false }
+    return true
+}
+
+
+function update() {
+    if (updatePost() || updateHexo()) {
+        shell.cd('/hexo/')
+        shell.exec('hexo clean && hexo d -g && hexo gulp')
+    }
+}
+
+setInterval(() => {
+    update()
+}, 5000)
